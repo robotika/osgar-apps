7c24d6ab1af93ad20d87e3fe12bed5d76688d126
Martin Dlouhy <md@robotika.cz>
2024-05-17 14:20:02 +0200

    log2map.py - improve map drawing (interactive)

--- robotem-rovne/log2map.py
+++ robotem-rovne/log2map.py
@@ -40,17 +40,56 @@ def create_map(logfile, stream_lidar, stream_odom, outfile,
     return poses, scans
 
 
+def get_xy_for_scan(pose, scan):
+    x0, y0, a0 = pose
+    x = [x0 / 1000.0 + math.cos(math.radians(a0 / 100 + 180 - 360 * i / 1800)) * dist / 1000.0 for i, dist in
+         enumerate(scan)]
+    y = [y0 / 1000.0 + math.sin(math.radians(a0 / 100 + 180 - 360 * i / 1800)) * dist / 1000.0 for i, dist in
+         enumerate(scan)]
+    return x, y
+
 def draw(poses, scans):
     import matplotlib.pyplot as plt
+    from matplotlib.widgets import Slider
+
+    fig, ax = plt.subplots()
+
+    scan_i = 0
+
     x = [p[0]/1000.0 for p in poses]
     y = [p[1]/1000.0 for p in poses]
-    plt.plot(x, y, '-o', color='orange')
-    for pose, scan in zip(poses, scans):
-        x0, y0, a0 = pose
-        x = [x0/1000.0 + math.cos(math.radians(a0/100 + 180 - 360 * i/1800)) * dist / 1000.0 for i, dist in enumerate(scan)]
-        y = [y0/1000.0 + math.sin(math.radians(a0/100 + 180 - 360 * i/1800)) * dist / 1000.0 for i, dist in enumerate(scan)]
-        plt.scatter(x, y, color='blue')
-    plt.axes().set_aspect('equal', 'datalim')
+    ax.plot(x, y, '-o', color='orange')
+
+    x, y = get_xy_for_scan(poses[0], scans[0])
+    scan_xy, = ax.plot(x, y, 'o', color='blue')
+
+    current, = ax.plot([poses[0][0]/1000.0], [poses[0][1]/1000.0], 'o', color='red')
+    ax.axis('equal')
+
+    # adjust the main plot to make room for the sliders
+    fig.subplots_adjust(bottom=0.25)
+
+    # recttuple (left, bottom, width, height)
+    # The dimensions (left, bottom, width, height) of the new Axes. All quantities are in fractions of figure width and height.
+    axfreq = fig.add_axes([0.1, 0.1, 0.8, 0.03])
+    freq_slider = Slider(
+        ax=axfreq,
+        label='Frame',
+        valmin=0,
+        valmax=len(scans) - 1,
+        valinit=scan_i,
+        valstep=1,
+    )
+
+    def update(val):
+        scan_i = int(freq_slider.val)
+        x, y = get_xy_for_scan(poses[scan_i], scans[scan_i])
+        scan_xy.set_xdata(x)
+        scan_xy.set_ydata(y)
+        current.set_xdata([poses[scan_i][0]/1000.0])
+        current.set_ydata([poses[scan_i][1]/1000.0])
+
+    freq_slider.on_changed(update)
     plt.show()
 
 
