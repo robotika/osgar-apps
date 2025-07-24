259ced4ce9562792f6f0952734947f4ad9bcbb79
Martin Dlouhy <md@robotika.cz>
2025-07-24 17:45:17 +0200

    provide input arguments and change map zoom limits

--- dtc-systems/dtc_report/organize_tiles.py
+++ dtc-systems/dtc_report/organize_tiles.py
@@ -1,43 +1,56 @@
 import os
 import shutil
+import argparse
 
-# --- Configuration ---
-source_folder = 'downloaded_tiles'
-destination_folder = 'tiles'
-# ---------------------
 
-if not os.path.exists(source_folder):
-    print(f"Error: Source folder '{source_folder}' not found.")
-    exit()
+def main():
+    parser = argparse.ArgumentParser(description='Convert downloaded images to tiles structure')
+    parser.add_argument('--src', required=True, help='Impot directory, where new tiles are downloaded')
+    parser.add_argument('--tiles-root', help='root directory where is the tree of tiles stored',
+                        default='tiles')
+    args = parser.parse_args()
 
-print(f"Starting tile organization from '{source_folder}'...")
+    # --- Configuration ---
+    source_folder = args.src
+    destination_folder = args.tiles_root
+    # ---------------------
 
-# Loop through all files in the source folder
-for filename in os.listdir(source_folder):
-    # Skip directories or files with extensions
-    if os.path.isdir(os.path.join(source_folder, filename)) or '.' in filename:
-        continue
+    if not os.path.exists(source_folder):
+        print(f"Error: Source folder '{source_folder}' not found.")
+        exit()
 
-    if '(' in filename:  # some strange PNG bits
-        continue
+    print(f"Starting tile organization from '{source_folder}'...")
 
-    # Split the name by the hyphen, e.g., "18-70083-106019" -> ["18", "70083", "106019"]
-    parts = filename.split('-')
+    # Loop through all files in the source folder
+    for filename in os.listdir(source_folder):
+        # Skip directories or files with extensions
+        if os.path.isdir(os.path.join(source_folder, filename)) or '.' in filename:
+            continue
 
-    if len(parts) == 3:
-        z, x, y = parts
-        
-        # Create the target directory path, e.g., "tiles/18/70083"
-        target_dir = os.path.join(destination_folder, z, x)
-        os.makedirs(target_dir, exist_ok=True)
-        
-        # Define the source and destination paths for the file
-        source_path = os.path.join(source_folder, filename)
-        # Add the .jpeg extension to the destination filename
-        destination_path = os.path.join(target_dir, y + '.jpeg')
-        
-        # Move and rename the file
-        shutil.copy(source_path, destination_path)
-        print(f"Copy {filename} -> {destination_path}")
+        if '(' in filename:  # some strange PNG bits
+            continue
 
-print("Tile organization complete.")
+        # Split the name by the hyphen, e.g., "18-70083-106019" -> ["18", "70083", "106019"]
+        parts = filename.split('-')
+
+        if len(parts) == 3:
+            z, x, y = parts
+
+            # Create the target directory path, e.g., "tiles/18/70083"
+            target_dir = os.path.join(destination_folder, z, x)
+            os.makedirs(target_dir, exist_ok=True)
+
+            # Define the source and destination paths for the file
+            source_path = os.path.join(source_folder, filename)
+            # Add the .jpeg extension to the destination filename
+            destination_path = os.path.join(target_dir, y + '.jpeg')
+
+            # Move and rename the file
+            shutil.copy(source_path, destination_path)
+            print(f"Copy {filename} -> {destination_path}")
+
+    print("Tile organization complete.")
+
+
+if __name__ == "__main__":
+    main()
