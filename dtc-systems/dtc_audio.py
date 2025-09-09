58500c6cd428a677238dc2c6cb466e77359baeb7
Martin Dlouhy <md@robotika.cz>
2025-09-09 20:37:33 +0200

    add dtc_audio.py

---
+++ dtc-systems/dtc_audio.py
@@ -0,0 +1,35 @@
+"""
+   DTC audio player
+"""
+from subprocess import call
+from osgar.node import Node
+
+
+class DTCAudio(Node):
+    def __init__(self, config, bus):
+        super().__init__(config, bus)
+        bus.register('playing')
+
+    def on_trigger(self, data):
+        self.publish('playing', True)
+        print('Playing ...')
+        call('ffplay -nodisp sounds/can_you_hear_me.mp3 -autoexit'.split())
+        print('... finished.')
+        self.publish('playing', False)
+
+
+def self_test():
+    from unittest.mock import MagicMock
+    bus = MagicMock()
+    audio_player = DTCAudio(bus=bus, config={})
+    audio_player.on_trigger(None)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='DTC Audio - test')
+    args = parser.parse_args()
+    self_test()
+
+# vim: expandtab sw=4 ts=4
