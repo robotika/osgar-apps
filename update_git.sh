6da1190bb295820e7e2f46fae08ee93886d58c10
Martin Dlouhy <md@robotika.cz>
2025-09-30 21:14:10 -0400

    ../update_git.sh - freeze matty and osgar repos - commented out

--- update_git.sh
+++ update_git.sh
@@ -7,11 +7,11 @@ fi
 
 echo "Robot IP is: $1"
 
-cd ~/git/matty && git push robot@$1:/home/robot/git/bare/matty.git
-ssh robot@$1 "cd /home/robot/git/matty && git pull"
+#cd ~/git/matty && git push robot@$1:/home/robot/git/bare/matty.git
+#ssh robot@$1 "cd /home/robot/git/matty && git pull"
 
-cd ~/git/osgar && git push robot@$1:/home/robot/git/bare/osgar.git
-ssh robot@$1 "cd /home/robot/git/osgar && git pull"
+#cd ~/git/osgar && git push robot@$1:/home/robot/git/bare/osgar.git
+#ssh robot@$1 "cd /home/robot/git/osgar && git pull"
 
 cd ~/git/osgar-apps && git push robot@$1:/home/robot/git/bare/osgar-apps.git
 ssh robot@$1 "cd /home/robot/git/osgar-apps && git pull"
