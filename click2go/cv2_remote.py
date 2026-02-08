09aff572a266d840f4a7b76a0fc78347357de165
Martin Dlouhy <md@robotika.cz>
2026-02-08 12:24:43 +0100

    add logging to cv2_remote.py

--- click2go/cv2_remote.py
+++ click2go/cv2_remote.py
@@ -6,6 +6,7 @@ import zmq
 import numpy as np
 
 from osgar.lib.serialize import serialize, deserialize
+from osgar.logger import LogWriter
 
 DOWNSCALE = 2
 
@@ -37,55 +38,62 @@ def start_cv_client(server_ip):
     pose2d = [0, 0, 0]
     print("Client started. Press 'q' to exit.")
 
-    try:
-        index = 0
-        robot_index = None
-        while True:
-            try:
-                # This will now only block for 1 second
-#                jpg_buffer = sub_socket.recv()
-                channel, raw = sub_socket.recv_multipart()
-                robot_index, robot_time_sec, pose2d, jpg_buffer = deserialize(raw)
-                print(f'Received {channel} {len(jpg_buffer)} data!')
-                
-                nparr = np.frombuffer(jpg_buffer, np.uint8)
-                img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-                if img is None:
-                    # try decode video instead
-                    with open('tmpX.h26x', 'wb') as f:
-                        f.write(jpg_buffer)
-                    cap = cv2.VideoCapture('tmpX.h26x')
-                    ret, img = cap.read()
-                    cap.release()
+    with LogWriter('base-viewer-') as log:
+        print(f'Logging into {log.filename}')
+        fromrobot_id = log.register('fromrobot')
+        torobot_id = log.register('torobot')
+        try:
+            index = 0
+            robot_index = None
+            while True:
+                try:
+                    # This will now only block for 1 second
+    #                jpg_buffer = sub_socket.recv()
+                    channel, raw = sub_socket.recv_multipart()
+                    log.write(fromrobot_id, raw)
+                    robot_index, robot_time_sec, pose2d, jpg_buffer = deserialize(raw)
+                    print(f'Received {channel} {len(jpg_buffer)} data!')
+
+                    nparr = np.frombuffer(jpg_buffer, np.uint8)
+                    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                    if img is None:
+                        # try decode video instead
+                        with open('tmpX.h26x', 'wb') as f:
+                            f.write(jpg_buffer)
+                        cap = cv2.VideoCapture('tmpX.h26x')
+                        ret, img = cap.read()
+                        cap.release()
+                        if img is not None:
+                            h, w = img.shape[:2]
+                            img = cv2.resize(img, (w//DOWNSCALE, h//DOWNSCALE))
+
                     if img is not None:
-                        h, w = img.shape[:2]
-                        img = cv2.resize(img, (w//DOWNSCALE, h//DOWNSCALE))
+                        cv2.imshow(window_name, img)
 
-                if img is not None:
-                    cv2.imshow(window_name, img)
-                    
-            except zmq.Again:
-                # This block hits if the 1s timeout is reached
-                print("No image received in the last second... waiting.")
-                # Optional: display a 'Connection Lost' overlay on the last frame
-            
-            # This part now runs even if no image was received
-            if pending_click:
-#                push_socket.send_string(pending_click)
-                data = pending_click.copy()
-                raw = serialize([index, robot_index, pose2d, [x*DOWNSCALE for x in data]])
-                index += 1
-                push_socket.send_multipart([bytes('cmd', 'ascii'), raw])
-                pending_click = None
+                except zmq.Again:
+                    # This block hits if the 1s timeout is reached
+                    print("No image received in the last second... waiting.")
+                    # Optional: display a 'Connection Lost' overlay on the last frame
 
-            # Crucial: cv2.waitKey handles the window refresh
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-    finally:
-        cv2.destroyAllWindows()
-        sub_socket.close()
-        push_socket.close()
-        context.term()
+                # This part now runs even if no image was received
+                if pending_click:
+    #                push_socket.send_string(pending_click)
+                    data = pending_click.copy()
+                    raw = serialize([index, robot_index, pose2d, [x*DOWNSCALE for x in data]])
+                    log.write(torobot_id, raw)
+                    index += 1
+                    push_socket.send_multipart([bytes('cmd', 'ascii'), raw])
+                    pending_click = None
+
+                # Crucial: cv2.waitKey handles the window refresh
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+        finally:
+            cv2.destroyAllWindows()
+            sub_socket.close()
+            push_socket.close()
+            context.term()
+
 
 if __name__ == "__main__":
     import argparse
