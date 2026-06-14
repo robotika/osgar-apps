dd15508c76b1b0d3bb6bfbf2122fd055980773b4
Martin Dlouhy <md@robotika.cz>
2026-06-14 07:45:48 +0200

    tulak ready for test

--- followme/tulak.py
+++ followme/tulak.py
@@ -15,58 +15,17 @@ from osgar.bus import BusShutdownException
 from osgar.exceptions import EmergencyStopException
 
 
-class AprilTag(Node):
-    def __init__(self, config, bus):
-        super().__init__(config, bus)
-        bus.register('apriltags', 'targets')
-        self.codec = av.CodecContext.create('hevc', 'r')  # h265
+def mask_center(mask):
+    if mask.max() == 0:
+        return mask.shape[0]//2, mask.shape[1]//2
+    assert mask.max() == 1, mask.max()
+    indices = np.argwhere(mask == 1)  # shape (num_points, 2)
 
-    def detect_april_tags(self, image):
-        dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_APRILTAG_25h9)
-        parameters = cv2.aruco.DetectorParameters()
-        detector = cv2.aruco.ArucoDetector(dictionary, parameters)
-        markerCorners, markerIds, rejectedCandidates = detector.detectMarkers(image)
-        if markerCorners is None or markerIds is None:
-            return [], []
-        assert len(markerCorners) == len(markerIds), (markerCorners, markerIds)
-        return [[int(x[0]) for x in markerIds],
-                [[[int(a), int(b)] for a, b in x[0]] for x in markerCorners]]
-
-    def corners_to_dist(self, corners):
-        center_x = sum([x for x, _ in corners])/4.0
-        center_y = sum([y for _, y in corners])/4.0
-        size = sum([math.hypot(x - center_x, y - center_y) for x, y in corners])/4.0
-        return 1.3 * 35.0/size  # GG factor
-
-    def corners_to_angle(self, corners):
-        center_x = sum([x for x, _ in corners])/4.0
-        width = 1920
-        return math.radians(69/2)*(width/2 - center_x)/(width/2)
-
-    def on_video(self, data):
-        try:
-            packets = self.codec.parse(data)
-            for packet in packets:
-                try:
-                    frames = self.codec.decode(packet)
-                    for frame in frames:
-                        img = frame.to_ndarray(format='bgr24')
-                        if img is not None:
-                            tags = self.detect_april_tags(img)
-                            if len(tags[0]) > 0:
-                                print(self.time, tags, [self.corners_to_dist(c) for c in tags[1]])
-                            self.publish('apriltags', tags)
-                            targets = [[self.corners_to_dist(c), self.corners_to_angle(c)] for c in tags[1]]
-                            self.publish('targets', targets)
-                except av.error.FFmpegError:
-                    # Ignore decoding errors from incomplete packets/keyframes at startup
-                    pass
-        except av.error.FFmpegError:
-            # Ignore parsing errors from incomplete streams
-            pass
+    # Compute the center of mass as the mean of these indices
+    return tuple(int(x) for x in indices.mean(axis=0))
 
 
-class FollowAprilTag(Node):
+class Tulak(Node):
     def __init__(self, config, bus):
         super().__init__(config, bus)
         bus.register('desired_steering')
@@ -75,8 +34,10 @@ class FollowAprilTag(Node):
         self.max_speed = config.get('max_speed', 0.5)
         self.target_distance = config.get('target_distance', 0.5)
         self.Kp_distance = config.get('Kp_distance', 0.5)
-        self.Kp_steering = config.get('Kp_steering', 2.0)
         self.raise_exception_on_stop = config.get('terminate_on_stop', True)
+
+        self.last_nn_mask = None
+        self.last_dir = 0  # straight
 
     def send_speed_cmd(self, speed, steering_angle):
         return self.bus.publish(
@@ -104,13 +65,31 @@ class FollowAprilTag(Node):
 
     def on_targets(self, data):
         if len(data) == 0:
-            self.send_speed_cmd(0, 0)
+            # fallback to nn-mask
+            speed, steering_angle = self.max_speed, self.last_dir
+            self.send_speed_cmd(speed, steering_angle)
         else:
             dist, angle = data[0]  # TODO select nearest
             if dist < self.target_distance:
                 self.send_speed_cmd(0, 0)
             else:
                 self.send_speed_cmd(self.max_speed, angle)
+
+    def on_nn_mask(self, data):
+        self.last_nn_mask = data.copy()  # make sure you modify only own copy
+        #        assert self.last_nn_mask.shape == (120, 160), self.last_nn_mask.shape
+        height, width = self.last_nn_mask.shape
+        self.last_nn_mask[:height // 2, :] = 0  # remove sky detections
+
+        center_y, center_x = mask_center(self.last_nn_mask)
+        dead = width // 16  # was 10
+        turn_angle = math.radians(20)
+        if center_x > width // 2 + dead:
+            self.last_dir = -turn_angle
+        elif center_x < width // 2 - dead:
+            self.last_dir = turn_angle
+        else:
+            self.last_dir = 0  # straight
 
     def on_depth(self, data):
         pass
