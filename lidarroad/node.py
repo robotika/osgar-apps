08673d21ba618d26dd3a9538862d14b43754b841
Martin Dlouhy <md@robotika.cz>
2026-08-01 05:32:20 +0200

    version 0

--- lidarroad/node.py
+++ lidarroad/node.py
@@ -1,11 +1,14 @@
+import numpy as np
+
 from osgar.node import Node
+from lidarroad.lidarroad import analyze_scan
 
 
 class LidarRoad(Node):
     def __init__(self, config, bus):
         super().__init__(config, bus)
-#        bus.register('desired_steering')
-        bus.register('desired_speed')
+        bus.register('desired_steering')
+        self.max_speed = config.get('max_speed', 0.1)
 
     def on_emergency_stop(self, data):
         pass
@@ -14,4 +17,19 @@ class LidarRoad(Node):
         pass
 
     def on_scan(self, data):
-        pass
+        assert len(data) == 1800, len(data)
+        _, from_i, to_i = analyze_scan(data, fast=True)
+        direction_i = (from_i + to_i) // 2
+        width = 100
+        safe_dist = 1.0
+        selection = np.array(data[direction_i-width:direction_i+width], dtype=int)
+        mask = selection != 0
+        if np.any(mask):
+            near_obstacle = np.percentile(selection[mask], 5) / 1000.0
+        else:
+            near_obstacle = 0.0
+        direction_deg = 360 * (900 - direction_i) / 1800
+        if self.verbose:
+            print(self.time, direction_deg, near_obstacle)
+        speed = self.max_speed if near_obstacle < safe_dist else 0
+        self.publish('desired_steering', [speed, int(direction_deg * 100)])
