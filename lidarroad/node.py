dcff02ad68806122344b598e4e8aaaa9d1193043
Martin Dlouhy <md@robotika.cz>
2026-08-01 05:01:08 +0200

    dh26 add skeleton

--- lidarroad/node.py
+++ lidarroad/node.py
@@ -1,5 +1,17 @@
-import osgar
+from osgar.node import Node
 
 
-class LidarRoad(osgar.node.Node):
-    pass
+class LidarRoad(Node):
+    def __init__(self, config, bus):
+        super().__init__(config, bus)
+#        bus.register('desired_steering')
+        bus.register('desired_speed')
+
+    def on_emergency_stop(self, data):
+        pass
+
+    def on_pose2d(self, data):
+        pass
+
+    def on_scan(self, data):
+        pass
