f26dd390a74d1a52d7cf9d898f123b70279d137d
Martin Dlouhy <md@robotika.cz>
2026-08-03 10:42:32 +0200

    Dobyvani hradu 2026 (#35)
    * add config/matty-on-pat-redroad.json and config/pat-remote-zmq.json
    * add lidarroad.node
    * search front only and add safety check

---
+++ lidarroad/node.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+from osgar.node import Node
+from osgar.exceptions import EmergencyStopException
+
+from lidarroad.lidarroad import analyze_scan
+
+
+class LidarRoad(Node):
+    def __init__(self, config, bus):
+        super().__init__(config, bus)
+        bus.register('desired_steering')
+        self.max_speed = config.get('max_speed', 0.1)
+
+    def on_emergency_stop(self, data):
+        if data:
+            raise EmergencyStopException()
+
+    def on_pose2d(self, data):
+        pass
+
+    def on_scan(self, data):
+        assert len(data) == 1800, len(data)
+        _, from_i, to_i = analyze_scan(data[450:-450], fast=True)
+        direction_i = 450 + (from_i + to_i) // 2
+        width = 100
+        safe_dist = 1.0
+        selection = np.array(data[direction_i-width:direction_i+width], dtype=int)
+        mask = selection != 0
+        if np.any(mask):
+            near_obstacle = np.percentile(selection[mask], 5) / 1000.0
+        else:
+            near_obstacle = 0.0
+        direction_deg = 360 * (900 - direction_i) / 1800
+        speed = self.max_speed if near_obstacle > safe_dist else 0
+        if self.verbose:
+            print(self.time, from_i, to_i, direction_deg, speed, near_obstacle)
+        self.publish('desired_steering', [int(1000 * speed), int(direction_deg * 100)])
